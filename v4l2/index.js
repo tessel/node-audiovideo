b95010c08d15f317147817ca13b530a1099337c1
Tim Cameron Ryan <id@timryan.org>
2015-05-27 01:15:28 -0700

    Fixes binary path lookup.

--- v4l2/index.js
+++ v4l2/index.js
@@ -1,7 +1,10 @@
-var raw = require("../build/Release/capture");
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')));
+var binding = require(binding_path);
 
 exports.Camera = function Camera() {
     var args = [null].concat([].slice.call(arguments));
-    var ctor = raw.Camera.bind.apply(raw.Camera, args);
+    var ctor = binding.Camera.bind.apply(binding.Camera, args);
     return new ctor;
 };
